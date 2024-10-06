c52004d8cfbcc5fea48b4987a3e71ec0b39d6d99
Rachit Chandak <rachitchandak2003@gmail.com>
2024-10-07 01:06:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Dictionary to store the latest sensor data
 sensor_data = {}
 
 @app.route('/')
@@ -16,9 +15,9 @@ def latest_data():
 @app.route('/data', methods=['POST'])
 def update_data():
     global sensor_data
-    sensor_data = request.json  # Update the global sensor_data with the incoming JSON
+    sensor_data = request.json 
     print("Data received:", sensor_data)
     return jsonify({"status": "success"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)  # Make sure to use port 80 for public access
+    app.run(host='0.0.0.0', port=80, debug=False) 
